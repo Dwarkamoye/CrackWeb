dfa2468f23c7b2c09de7f04af9f32449e530a812
Dwarkamoye Mohanty <mdwarkamoye@gmail.com>
2019-03-08 17:30:38 -0600

    GoogleKnowledgeSearch API

--- Lab_Assignment_2/GoogleKnowledge/src/app/lists/lists.component.ts
+++ Lab_Assignment_2/GoogleKnowledge/src/app/lists/lists.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ServicesService } from '../services/services.service';
 @Component({
   selector: 'app-lists',
   templateUrl: './lists.component.html',
   styleUrls: ['./lists.component.css']
 })
 export class ListsComponent implements OnInit {
-
-  constructor() { }
-
+  listsdata = {};
+  constructor(private service : ServicesService) { }
   ngOnInit() {
+    this.listsdata=this.service.getData();
+    console.log(this.listsdata);
   }
-
+  itemClicked(item){
+    window.open(item.result.detailedDescription.url, '_blank',
+      'toolbar=yes,scrollbars=yes,resizable=yes,top=0,left=100,width=1200,height=800');
+  }
 }
--- Lab_Assignment_2/GoogleKnowledge/src/app/show/show.component.ts
+++ Lab_Assignment_2/GoogleKnowledge/src/app/show/show.component.ts
@@ -1,35 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { ServicesService } from '../services/services.service';
 @Component({
   selector: 'app-show',
   templateUrl: './show.component.html',
   styleUrls: ['./show.component.css']
 })
 export class ShowComponent implements OnInit {
-data:any;
+  data: any;
   dropdownvalue: string;
-  constructor(
-    private http: HttpClient) {}
 
-  selectedLevel = {id: '10' , name: 'Person'};
-  entity: Array <object> = [
-    { id:1, name:'Book'},
-    { id:2 , name:'BookSeries'},
-    { id:3, name :'EducationalOrganization'},
-    { id:4, name:'WebSite'},
-    { id:5, name:'GovernmentOrganization'},
-    { id:6, name:'Movie'},
-    { id:7, name:'MovieSeries'},
-    { id:8, name:'MusicGroup'},
-    { id:9, name:'MusicRecording'},
-    { id:10, name:'Person'},
-    { id:11, name:'Place'},
-    { id:12, name:'SportsTeam'},
-    { id:13, name:'TVEpisode'},
-    { id:14, name:'TVSeries'},
-    { id:15, name:'VideoGame'}
-    ];
+  constructor(
+    private http: HttpClient, private router: Router, private service: ServicesService) {
+  }
+
+  limit = 10;
+  entity: Array<object> = [
+    {id: 1, name: 'Book'},
+    {id: 2, name: 'BookSeries'},
+    {id: 3, name: 'EducationalOrganization'},
+    {id: 4, name: 'WebSite'},
+    {id: 5, name: 'GovernmentOrganization'},
+    {id: 6, name: 'Movie'},
+    {id: 7, name: 'MovieSeries'},
+    {id: 8, name: 'MusicGroup'},
+    {id: 9, name: 'MusicRecording'},
+    {id: 10, name: 'Person'},
+    {id: 11, name: 'Place'},
+    {id: 12, name: 'SportsTeam'},
+    {id: 13, name: 'TVEpisode'},
+    {id: 14, name: 'TVSeries'},
+    {id: 15, name: 'VideoGame'}
+  ];
+
   selected(item) {
     this.dropdownvalue = item.name;
   }
@@ -37,15 +41,20 @@ data:any;
   ngOnInit() {
     // this.search();
   }
-search(){
-    alert(this.dropdownvalue);
-    const API_KEY = 'AIzaSyBCCDbKZPZiV5rCP8Uit3EHoaYQr8hDKgI';
+
+  search() {
+    const API_KEY = 'AIzaSyDoLoA-wOheATb0jJD0KgxMKyTxg4cc-0E';
     const inputValue = (document.getElementById('something') as HTMLInputElement).value;
     let url = 'https://kgsearch.googleapis.com/v1/entities:search?query=' + inputValue +
-      '&indent=true&languages=en&limit=3&prefix=true&types='+this.dropdownvalue + '&alt=json&key=' + API_KEY;
-    this.http.get(url).subscribe((data: any ) => {
-    this.data = data;
-    console.log(data);
-  });
+      '&indent=true&languages=en&limit=' + this.limit + '&prefix=true&types=' + this.dropdownvalue + '&alt=json&key=' + API_KEY;
+    this.service.gethttpData(url)
+      .subscribe((data: any) => {
+          console.log(data);
+          this.data = data.itemListElement;
+          this.service.setData(this.data);
+          this.router.navigateByUrl('/lists');
+        },
+        error => console.log(error) // error path);
+      );
   }
 }
